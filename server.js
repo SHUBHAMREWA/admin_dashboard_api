1ba7a8443bf2aacd373e8dbbaf0578b8efe2cf27
shubham <shubhamrewamp17@gmail.com>
2025-07-01 21:16:44 +0530

    headers removed

--- server.js
+++ server.js
@@ -13,16 +13,6 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://admin-dashboard-five-opal.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(200);
-});
-
-
-
 app.use(express.json());
 
 // .env setup 
